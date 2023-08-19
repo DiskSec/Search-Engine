f6b4d3c541f4cbd12bf5054a8ef152ccc84d9dd7
C71PPY <140841386+C71PPY@users.noreply.github.com>
2023-08-19 23:15:10 +0200

    Add files via upload

---
+++ search.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+def search(query):
+    search_url = f"https://www.search.brave.com/search?q={query}"
+    response = requests.get(search_url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        results = soup.find_all("h3", class_="result-title")
+        
+        for index, result in enumerate(results, start=1):
+            title = result.get_text()
+            print(f"{index}. {title}")
+    else:
+        print("Error fetching search results.")
+
+if __name__ == "__main__":
+    search_query = input("Enter your search query: ")
+    search(search_query)
